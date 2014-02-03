a38251e2a87763f92e91f820376c2682eeb56db8
Andrew Smith <andrew.smith@kneto.fi>
2014-02-03 09:51:47 +0200

    added get section and increased chars

--- hotel/models.py
+++ hotel/models.py
@@ -6,72 +6,103 @@ class HomePage(Page, RichText):
     '''
     A page representing the format of the home page
     '''
-    lefttop_header = models.CharField(max_length=100,
+    lefttop_header = models.CharField(max_length=150,
         help_text="left top header", default="header")
-    lefttop_line1 = models.CharField(max_length=50,
+    lefttop_line1 = models.CharField(max_length=100,
         help_text="line1", default="line1")
-    lefttop_line2 = models.CharField(max_length=50,
+    lefttop_line2 = models.CharField(max_length=100,
         help_text="line2", default="line2")
-    lefttop_line3 = models.CharField(max_length=50,
+    lefttop_line3 = models.CharField(max_length=100,
         help_text="line3", default="line3")
-    lefttop_line4 = models.CharField(max_length=50,
+    lefttop_line4 = models.CharField(max_length=100,
         help_text="line4", default="line4")
-    lefttop_line5 = models.CharField(max_length=50,
+    lefttop_line5 = models.CharField(max_length=100,
         help_text="line5", default="line5")
 
     righttop_header = models.CharField(max_length=100,
         help_text="right top header", default="header")
-    righttop_line1 = models.CharField(max_length=50,
+    righttop_line1 = models.CharField(max_length=75,
         help_text="line1", default="line1")
 
     leftbottom_header = models.CharField(max_length=100,
         help_text="left bottom header", default="header")
-    leftbottom_line1 = models.CharField(max_length=50,
+    leftbottom_line1 = models.CharField(max_length=75,
         help_text="line1", default="line1")
 
     rightbottom_header = models.CharField(max_length=100,
         help_text="right bottom header", default="header")
-    rightbottom_line1 = models.CharField(max_length=50,
+    rightbottom_line1 = models.CharField(max_length=75,
         help_text="line1", default="line1")
 
-    section1_header = models.CharField(max_length=50,
+    section1_header = models.CharField(max_length=75,
         help_text="section1 header", default="header text")
     section1 = models.TextField(max_length=2000,
         help_text="put body copy here", default="body copy")
 
-    section2_header = models.CharField(max_length=50,
+    section2_header = models.CharField(max_length=75,
         help_text="section1 header", default="header text")
     section2 = models.TextField(max_length=2000,
         help_text="put body copy here", default="body copy")
 
-    section3_header = models.CharField(max_length=50,
+    section3_header = models.CharField(max_length=75,
         help_text="section1 header", default="header text")
     section3 = models.TextField(max_length=2000,
         help_text="put body copy here", default="body copy")
 
-    get_header = models.CharField(max_length=50,
+    get_header = models.CharField(max_length=150,
         help_text="get header", default="header text")
-
-    get_header1 = models.CharField(max_length=50,
+    get_header1 = models.CharField(max_length=150,
         help_text="section1 header", default="header text")
     get_text1 = models.TextField(max_length=2000,
         help_text="put body copy here", default="body copy")
-    get_header2 = models.CharField(max_length=50,
+    get_header2 = models.CharField(max_length=150,
         help_text="section2 header", default="header text")
     get_text2 = models.TextField(max_length=2000,
         help_text="put body copy here", default="body copy")
-    get_header3 = models.CharField(max_length=50,
+    get_header3 = models.CharField(max_length=150,
         help_text="section3 header", default="header text")
     get_text3 = models.TextField(max_length=2000,
     	help_text="put body copy here", default="body copy")
-    get_header4 = models.CharField(max_length=50,
+    get_header4 = models.CharField(max_length=150,
         help_text="section4 header", default="header text")
     get_text4 = models.TextField(max_length=2000,
     	help_text="put body copy here", default="body copy")
-    get_header5 = models.CharField(max_length=50,
+    get_header5 = models.CharField(max_length=150,
         help_text="section4 header", default="header text")
     get_text5 = models.TextField(max_length=2000,
     	help_text="put body copy here", default="body copy")
+    get_header6 = models.CharField(max_length=150,
+        help_text="section4 header", default="header text")
+    get_text6 = models.TextField(max_length=2000,
+        help_text="put body copy here", default="body copy")
+
+
+    what_header = models.CharField(max_length=150,
+        help_text="what header", default="header text")
+    what_header1 = models.CharField(max_length=150,
+        help_text="section1 header", default="header text")
+    what_text1 = models.TextField(max_length=2000,
+        help_text="put body copy here", default="body copy")
+    what_header2 = models.CharField(max_length=150,
+        help_text="section2 header", default="header text")
+    what_text2 = models.TextField(max_length=2000,
+        help_text="put body copy here", default="body copy")
+    what_header3 = models.CharField(max_length=150,
+        help_text="section3 header", default="header text")
+    what_text3 = models.TextField(max_length=2000,
+        help_text="put body copy here", default="body copy")
+    what_header4 = models.CharField(max_length=150,
+        help_text="section4 header", default="header text")
+    what_text4 = models.TextField(max_length=2000,
+        help_text="put body copy here", default="body copy")
+    what_header5 = models.CharField(max_length=150,
+        help_text="section4 header", default="header text")
+    what_text5 = models.TextField(max_length=2000,
+        help_text="put body copy here", default="body copy")
+    what_header6 = models.CharField(max_length=150,
+        help_text="section4 header", default="header text")
+    what_text6 = models.TextField(max_length=2000,
+        help_text="put body copy here", default="body copy")
 
 
     class Meta:
