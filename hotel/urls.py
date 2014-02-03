56180a8aa658feafa994dac239b3f8f92ebf7945
Andrew Smith <andrew.smith@kneto.fi>
2014-02-03 18:23:18 +0200

    fist stab at zendesk auth

---
+++ hotel/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import handler404, handler500, patterns, include
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+#------------------------------------------------------------------------------
+urlpatterns = patterns('',
+
+    (r'^authorize/', 'hotel.views.index'),
+)
--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.conf import settings
+
+import time
+import jwt
+import uuid
+
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+@never_cache
+@login_required
+def index(request):
+
+  u = request.user
+
+  first = u.first_name
+  last = u.last_name
+  if not first and not last:
+    first = "Anonymous"
+    last = "User"
+
+  payload = {
+    "iat": int(time.time()),
+    "jti": str(uuid.uuid1()),
+    "name": first + last,
+    "email": request.user.email
+  }
+
+  subdomain = settings.ZENDESK_SUBDOMAIN
+  shared_key = settings.ZENDESK_TOKEN
+  jwt_string = jwt.encode(payload, shared_key)
+  return HttpResponseRedirect("https://" + subdomain + ".zendesk.com/access/jwt?jwt=" + jwt_string)
