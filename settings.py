b0d81978adca5eb1a555a80088bba08263a5c646
Andrew Smith <andrew.smith@kneto.fi>
2014-02-03 09:39:14 +0200

    removed mobil

--- settings.py
+++ settings.py
@@ -250,7 +250,7 @@ INSTALLED_APPS = (
     "mezzanine.galleries",
     #"mezzanine.twitter",
     "mezzanine.accounts",
-    "mezzanine.mobile",
+ #   "mezzanine.mobile",
 )
 
 # List of processors used by RequestContext to populate the context.
